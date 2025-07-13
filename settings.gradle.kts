423269d014d673e25c26b8297ba2fac7ad524b20
Manel Martos <manel.martos@wikiloc.com>
2025-07-13 21:30:36 +0200

    Integrate refreshVersions plugin and update dependencies configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,20 @@ pluginManagement {
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositories {
+      google()
+      mavenCentral()
+  }
+}
+
+plugins {
+  // See https://splitties.github.io/refreshVersions
+  id("de.fayard.refreshVersions") version "0.60.5"
+}
+
+refreshVersions {
+  file("build/tmp/refreshVersions").mkdirs()
+  versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties")
 }
