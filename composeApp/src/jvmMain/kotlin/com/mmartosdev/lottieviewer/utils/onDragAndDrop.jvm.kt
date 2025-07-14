c66915c5cc944aac2aca61b2c9286d541da9f251
Manel Martos <manel.martos@wikiloc.com>
2025-07-14 21:17:11 +0200

    Refactor drag-and-drop implementation to use `dragAndDropTarget` API and optimize Lottie animation initialization logic.

--- composeApp/src/jvmMain/kotlin/com/mmartosdev/lottieviewer/utils/onDragAndDrop.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/mmartosdev/lottieviewer/utils/onDragAndDrop.jvm.kt
@@ -1,35 +1,61 @@
 package com.mmartosdev.lottieviewer.utils
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.draganddrop.dragAndDropTarget
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.DragData
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.onExternalDrag
+import androidx.compose.ui.composed
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.DragData
+import androidx.compose.ui.draganddrop.dragData
 import com.mmartosdev.lottieviewer.data.FileDesc
 import java.net.URI
 
 data class UriFileDesc(val uri: URI) : FileDesc
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 actual fun Modifier.onDragAndDrop(
-    onDragStart: () -> Unit,
-    onDragExit: () -> Unit,
-    onSingleFileDropped: (FileDesc) -> Unit,
-): Modifier = this then onExternalDrag(
-    enabled = true,
-    onDrag = { },
-    onDrop = { extractSingleFile(it.dragData)?.run { onSingleFileDropped(this) } },
-    onDragExit = { onDragExit() },
-    onDragStart = { onDragStart() },
-)
+  onDragStart: () -> Unit,
+  onDragExit: () -> Unit,
+  onSingleFileDropped: (FileDesc) -> Unit,
+): Modifier = this.composed {
+  val dragAndDropTarget: DragAndDropTarget = remember {
+    object : DragAndDropTarget {
+      override fun onDrop(event: DragAndDropEvent): Boolean =
+        (extractSingleFile(event.dragData())?.run {
+          onSingleFileDropped(this)
+          true
+        } ?: false).also {
+          println("onDrop")
+        }
+
+      override fun onChanged(event: DragAndDropEvent) {
+        println("onChanged")
+      }
+
+      override fun onEntered(event: DragAndDropEvent) {
+        println("onEntered")
+      }
+
+      override fun onExited(event: DragAndDropEvent) {
+        println("onExited")
+      }
+    }
+  }
+  dragAndDropTarget(shouldStartDragAndDrop = { true }, target = dragAndDropTarget)
+}
+
 
 @OptIn(ExperimentalComposeUiApi::class)
 private fun extractSingleFile(dragData: DragData): FileDesc? =
-    (dragData as? DragData.FilesList)?.run {
-        val files = readFiles()
-        if (files.size == 1) {
-            return UriFileDesc(URI.create(files[0]))
-        }
-        null
+  (dragData as? DragData.FilesList)?.run {
+    val files = readFiles()
+    if (files.size == 1) {
+      return UriFileDesc(URI.create(files[0]))
     }
+    null
+  }
